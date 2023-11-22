7d602334da7aff39acfba8a7a6608a7c92932bd4
Cristian Peralta <cbperaltasegura@gmail.com>
2023-11-21 19:11:46 -0500

    refact: Refactor listTables and addUser methods to handle endpoints, process request and return a response

--- dynamodb.js
+++ dynamodb.js
@@ -12,22 +12,41 @@ const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
 
 const TABLE_USERS = "crud-api-users";
 
-const listTables = () => {
-    ddb.listTables({ Limit:10 }, function(err, data) {
-        if (err) {
-            console.log("Error", err.code);
-        } else {
-            console.log("Tables names are ", data.TableNames);
-        }
-    });
+const listTables = async (req, res) => {
+    try {
+        const tables = await ddb.listTables({ Limit: 10 }).promise();
+        return res.status(200).json({ tables: tables.TableNames });
+    } catch (error) {
+        res.status(error.code || 400).json({
+            errors: {
+                msg: error.message,
+            },
+        });
+    }
 };
 
-const addUser = async (user) => {
-    const params = {
-        TableName: TABLE_USERS,
-        Item: user
-    };
-    return await dynamoClient.put(params).promise();
+const addUser = async (req, res) => {
+    try {
+        const { name, lastName } = req.body;
+        const date = new Date();
+        const id = String(date.getTime());
+        const params = {
+            TableName: TABLE_USERS,
+            Item: {
+                id,
+                name,
+                lastName
+            }
+        };
+        const newItem = await dynamoClient.put(params).promise();
+        return res.status(200).json({ data: newItem });
+    } catch (error) {
+        res.status(error.code || 400).json({
+            errors: {
+                msg: error.message,
+            },
+        });
+    }
 };
 
 module.exports = {
